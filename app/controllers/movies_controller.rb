6b1d67d9402f4ce0d650440293b1e0d1082e4851
Jeff Cohen <cohen.jeff@gmail.com>
2016-04-15 20:18:48 -0500

    Routing syntax

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,8 @@
 class MoviesController < ApplicationController
 
+
+  def secret
+    redirect_to "http://localhost:3000/poker"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get("/secret", { :controller => "movies", :action => "secret" })
   get("/movies", { :controller => "movies", :action => "list" })
-  get("/poker", { :controller => "cards", :action => "deal" })
+
+  #get "/poker", :controller => "cards", :action => "deal"
+  get "/poker" => "cards#deal"
+
+
 
 end
