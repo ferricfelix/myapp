9e57e8a06aa36e7f85de56912a91be89db512a59
Jeff Cohen <cohen.jeff@gmail.com>
2016-04-16 15:12:16 -0500

    Dynamic segments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "/movies" => 'movies#index'
   get "/poker" => "cards#deal"
 
+  get '/movies/about' => 'movies#about'
+  get '/movies/:id' => 'movies#show'
+
   get '/movie_details' => 'movies#show'
 
 end
